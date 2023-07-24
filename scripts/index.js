9650a26540d25cc5ef7e14b49e8591bf593d89fc
Hung <macbook@Hungs-MacBook-Pro.local>
2023-07-24 14:30:17 +0700

    Add comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
 
+// Each allowlist/whitelist will have a list of addresses and a merkle root, we will save these values to the database
+
+// Save this list to the database
 const values = [
     ["0x4B741235f451474CEAC5a1F4ADa56523618D9292"],
     ["0xbE3Af7D88C85240ba3A7a7d4DCCFB62F61eb3D85"],
@@ -10,12 +13,14 @@ const values = [
 
 const tree = StandardMerkleTree.of(values, ["address"]);
 
+// Use this to check allowlist/whitelist for a address
+console.log('Verify Proof of a value:', tree.verify(values[1], tree.getProof(values[1])));
+
+// Save this values to db
+console.log('Merkle Root:', tree.root);
+
 console.log('Tree:', tree);
 
 console.log('Proof of a value:', tree.getProof(values[1]));
-
-console.log('Verify Proof of a value:', tree.verify(values[1], tree.getProof(values[1])));
-
-console.log('Merkle Root:', tree.root);
 
 console.log("Leaf hash:", tree.leafHash(values[1]));
